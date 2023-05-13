eb8fc1e2dbc43a9c7043274fc27c04854c906952
MOHAMMED ABUJAYYAB <115325019+mohajayyab@users.noreply.github.com>
2023-05-13 14:13:35 +0200

    add nuget packages and mdf , ldf files

--- HXINTL_HFT_2022232.Data/Class1.cs
+++ HXINTL_HFT_2022232.Data/Class1.cs
@@ -4,5 +4,7 @@ namespace HXINTL_HFT_2022232.Data
 {
     public class Class1
     {
+        // Data Source=(LocalDB)\MSSQLLocalDB; AttachDbFilename="|DataDirectory|\HXINTLDatabase.mdf"; Integrated Security=True; MultipleActiveResultSets=True
+
     }
 }
