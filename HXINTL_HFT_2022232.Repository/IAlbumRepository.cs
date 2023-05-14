4ab4d11dc1ed4a0ee7aca9510a27dded6e27df7d
MOHAMMED ABUJAYYAB <115325019+mohajayyab@users.noreply.github.com>
2023-05-14 14:35:05 +0200

    Create IAlbumRepository.cs

---
+++ HXINTL_HFT_2022232.Repository/IAlbumRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HXINTL_HFT_2022232.Models;
+
+namespace HXINTL_HFT_2022232.Repository
+{
+    public interface IAlbumRepository
+    {
+        void Create(Album albumEvent);
+        void Delete(int Albumid);
+        IQueryable<Album> GetAll();
+        Album Read(int Albumid);
+        void Update(Album AlbumEvent);
+    }
+}
