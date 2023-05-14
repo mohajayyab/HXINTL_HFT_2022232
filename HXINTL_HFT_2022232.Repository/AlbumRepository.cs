6ab86d49c14740c1c641252fc3170dd69c070d26
MOHAMMED ABUJAYYAB <115325019+mohajayyab@users.noreply.github.com>
2023-05-14 21:58:08 +0200

    Create AlbumRepository.cs

---
+++ HXINTL_HFT_2022232.Repository/AlbumRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HXINTL_HFT_2022232.Models;
+using HXINTL_HFT_2022232.Data;
+
+namespace HXINTL_HFT_2022232.Repository
+{
+    class AlbumRepository : IAlbumRepository
+    {
+        protected readonly MusicLibraryContext db;
+        public AlbumRepository(MusicLibraryContext db)
+        {
+            this.db = db;
+        }
+        public void Create(Album album)
+        {
+            db.Albums.Add(album);
+            db.SaveChanges();
+        }
+        public Album Read(int Albumid)
+        {
+            return
+                db.Albums.FirstOrDefault(t => t.AlbumID == Albumid);
+        }
+        public IQueryable<Album> GetAll()
+        {
+            return db.Albums;
+        }
+        public void Delete(int Albumid)
+        {
+            var AlbumToDelete = Read(Albumid);
+            db.Albums.Remove(AlbumToDelete);
+            db.SaveChanges();
+        }
+        public void Update(Album album)
+        {
+            var AlbumToUpdate = Read(album.AlbumID);
+            AlbumToUpdate.Title = album.Title;
+            AlbumToUpdate.BasePrice = album.BasePrice;
+            db.SaveChanges();
+        }
+    }
+}
