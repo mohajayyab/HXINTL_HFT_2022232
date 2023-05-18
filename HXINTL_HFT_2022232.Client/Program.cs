c6d3268f209eee1e0fb8107fd920bd443bcb4ec2
MOHAMMED ABUJAYYAB <115325019+mohajayyab@users.noreply.github.com>
2023-05-18 20:25:31 +0200

    install console menu simple

--- HXINTL_HFT_2022232.Client/Program.cs
+++ HXINTL_HFT_2022232.Client/Program.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿using ConsoleTools;
+using HXINTL_HFT_2022232.Data;
+using HXINTL_HFT_2022232.Logic;
+using HXINTL_HFT_2022232.Models;
+using HXINTL_HFT_2022232.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace HXINTL_HFT_2022232.Client
 {
@@ -6,7 +15,14 @@ namespace HXINTL_HFT_2022232.Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MusicLibraryContext mlc = new MusicLibraryContext();
+            AlbumRepository albumRepo = new AlbumRepository(mlc);
+            TrackRepository trackRepo = new TrackRepository(mlc);
+            ArtistRepository artistRepo = new ArtistRepository(mlc);
+            TrackLogic trackLogic = new TrackLogic(trackRepo);
+            AlbumLogic albumLogic = new AlbumLogic(albumRepo);
+            ArtistLogic artistLogic = new ArtistLogic(artistRepo);
+
         }
     }
 }
