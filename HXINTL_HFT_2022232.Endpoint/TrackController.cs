6f3a781b4358fc77822832ec5f23ee124fadcc6c
MOHAMMED ABUJAYYAB <115325019+mohajayyab@users.noreply.github.com>
2023-05-18 19:03:06 +0200

    Create TrackController.cs

---
+++ HXINTL_HFT_2022232.Endpoint/TrackController.cs
@@ -0,0 +1,52 @@
+﻿using HXINTL_HFT_2022232.Endpoint;
+using HXINTL_HFT_2022232.Logic;
+using HXINTL_HFT_2022232.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
+
+namespace HXINTL_HFT_2022232.Endpoint
+{
+    public class TrackController : Controller
+    {
+        ITrackLogic t1;
+        public TrackController(ITrackLogic t1)
+        {
+            this.t1 = t1;
+
+        }
+        [HttpGet]
+        public IEnumerable<Track> Get()
+        {
+            return t1.GetTracks();
+        }
+
+        // GET /brand/5
+        [HttpGet("{id}")]
+        public Track Get(int TrackId)
+        {
+            return t1.GetTrack(TrackId);
+        }
+
+        // POST /brand
+        [HttpPost]
+        public void Post([FromBody] Track value)
+        {
+            t1.CreatTrack(value.TrackId, value.NamePlace, value.Length);
+        }
+
+        // PUT /brand
+        [HttpPut]
+        public void Put([FromBody] Track value)
+        {
+            t1.UpdateTrack(value);
+        }
+
+        // DELETE /brand/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            t1.DeleteTrack(id);
+        }
+    }
+}
