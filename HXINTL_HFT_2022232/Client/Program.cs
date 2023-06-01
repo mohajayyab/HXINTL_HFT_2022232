2ef5bfd502b406c6cf89f91f8877093886451d8d
mohammedabujayyab <115325019+mohajayyab@users.noreply.github.com>
2023-06-01 20:20:33 +0200

    rebulid the structure

---
+++ HXINTL_HFT_2022232/Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HXINTL_HFT_2022232.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
