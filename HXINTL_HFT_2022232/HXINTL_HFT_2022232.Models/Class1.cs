9b6aa3ba45cf0110bc09ae27487e304a15ea7183
mohammedabujayyab <115325019+mohajayyab@users.noreply.github.com>
2023-06-01 20:25:01 +0200

    update

--- HXINTL_HFT_2022232/HXINTL_HFT_2022232.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace HXINTL_HFT_2022232.Models
-{
-    public class Class1
-    {
-    }
-}
--- HXINTL_HFT_2022232/HXINTL_HFT_2022232.Repository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace HXINTL_HFT_2022232.Repository
-{
-    public class Class1
-    {
-    }
-}
