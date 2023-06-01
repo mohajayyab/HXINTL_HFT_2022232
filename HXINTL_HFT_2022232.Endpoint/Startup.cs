4955bdfd0b24df2a5165555500b55665a13968f9
mohammedabujayyab <115325019+mohajayyab@users.noreply.github.com>
2023-06-01 23:32:17 +0200

    Update Startup.cs

--- HXINTL_HFT_2022232.Endpoint/Startup.cs
+++ HXINTL_HFT_2022232.Endpoint/Startup.cs
@@ -1,21 +1,51 @@
-﻿using System;
+﻿using HXINTL_HFT_2022232.Logic;
+using HXINTL_HFT_2022232.Models;
+using HXINTL_HFT_2022232.Repository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace HXINTL_HFT_2022232.Endpoint
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<MotorsDBContext>();
+
+            services.AddTransient<IRepository<RentMotorcycle>, RentMotorcycleRepository>();
+            services.AddTransient<IRepository<Motorcycle>, MotorRepository>();
+            services.AddTransient<IRepository<Brand>, BrandRepository>();
+
+            services.AddTransient<IRentMotorLogic, RentMotorcycleLogic>();
+            services.AddTransient<IMotorcycleLogic, MotorcycleLogic>();
+            services.AddTransient<IBrandLogic, BrandLogic>();
+
+
+            services.AddCors();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "HXINTL_HFT_2022232.Endpoint", Version = "v1.0" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,18 +54,27 @@ namespace HXINTL_HFT_2022232.Endpoint
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HXINTL_HFT_2022232.Endpoint v1.0"));
             }
+
+            app.UseCors(x =>
+                x.AllowCredentials()
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .WithOrigins("http://localhost:13104")
+            );
+
+
 
             app.UseRouting();
 
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
 }
-
